c9d333492293d22eb61a06472d33b655afa2bd3e
wurikiji <wurikiji@gmail.com>
2018-04-09 16:05:27 +0900

    Add initial views

---
+++ src/ChatRoom.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+const welcomeText = "환영합니다 :)";
+
+
+class ChatArea extends Component {
+
+}
+
+class ChatBox extends Component {
+
+}
+
+class ChatRoom extends Component {
+    constructor (props) {
+
+    }
+
+    render() {
+        return (
+        <div>
+            
+        </div>
+        );
+    }
+}
+
+export default ChatRoom;
---
+++ src/WeatherView.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+const welcomeText = "환영합니다 :)";
+
+
+// 글씨를 통해 간단하게 요약 해주는 부분
+// 디자인에서 "어디서 무얼 하든 기분 좋은 날씨입니다" 부분
+class WeatherHeader extends Component {
+
+}
+
+// 날씨 표시하는 전체 컨테이너
+class WeatherView extends Component {
+    constructor (props) {
+
+    }
+
+    render() {
+        return (
+        <div>
+            
+        </div>
+        );
+    }
+}
+
+export default ChatRoom;
